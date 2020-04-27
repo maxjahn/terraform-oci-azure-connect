c5a8899034d8ccc19140b530c83aafa988a3d4c9
maxjahn <max.jahn@gmail.com>
2020-04-27 14:21:53 +0200

    updated to terraform 0.12

--- testing.tf
+++ testing.tf
@@ -1,28 +1,28 @@
 ### oci testing 
 
 data "oci_identity_availability_domains" "az_connect_adcomp" {
-  compartment_id = "${var.oci_compartment_ocid}"
+  compartment_id = var.oci_compartment_ocid
 }
 
 resource "oci_core_instance" "az_connect_test_instance" {
-  count = "${var.oci_test_instances_count}" 
-  availability_domain = "${lookup(data.oci_identity_availability_domains.az_connect_adcomp.availability_domains[0],"name")}"
-  compartment_id      = "${var.oci_compartment_ocid}"
+  count               = var.oci_test_instances_count
+  availability_domain = data.oci_identity_availability_domains.az_connect_adcomp.availability_domains[0]["name"]
+  compartment_id      = var.oci_compartment_ocid
   shape               = "VM.Standard2.1"
 
   create_vnic_details {
-    subnet_id              = "${oci_core_subnet.az_connect_subnet.id}"
+    subnet_id              = oci_core_subnet.az_connect_subnet.id
     skip_source_dest_check = true
   }
 
   display_name = "az-connect-test-instance-${count.index}"
 
-  metadata {
-    ssh_authorized_keys = "${var.ssh_public_key}"
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
   }
 
   source_details {
-    source_id   = "${var.oci_test_image}"
+    source_id   = var.oci_test_image
     source_type = "image"
   }
 
@@ -30,44 +30,43 @@ resource "oci_core_instance" "az_connect_test_instance" {
 }
 
 output "oci_vm_private_ip" {
-  value = ["${oci_core_instance.az_connect_test_instance.*.private_ip}"]
+  value = [oci_core_instance.az_connect_test_instance.*.private_ip]
 }
 
 output "oci_vm_public_ip" {
-  value = ["${oci_core_instance.az_connect_test_instance.*.public_ip}"]
+  value = [oci_core_instance.az_connect_test_instance.*.public_ip]
 }
-
 
 ### azure testing
 
 resource "azurerm_public_ip" "oci_test_ip" {
-  count = "${var.arm_test_instances_count}"
+  count               = var.arm_test_instances_count
   name                = "oci-test-ip-${count.index}"
-  location            = "${azurerm_resource_group.oci_connect.location}"
-  resource_group_name = "${azurerm_resource_group.oci_connect.name}"
+  location            = azurerm_resource_group.oci_connect.location
+  resource_group_name = azurerm_resource_group.oci_connect.name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "oci_testvm_nic" {
-  count = "${var.arm_test_instances_count}"
+  count               = var.arm_test_instances_count
   name                = "oci-testvm-nic-${count.index}"
-  location            = "${azurerm_resource_group.oci_connect.location}"
-  resource_group_name = "${azurerm_resource_group.oci_connect.name}"
+  location            = azurerm_resource_group.oci_connect.location
+  resource_group_name = azurerm_resource_group.oci_connect.name
 
   ip_configuration {
     name                          = "oci-testvm-nic-config"
-    subnet_id                     = "${azurerm_subnet.oci_connect_subnet.id}"
+    subnet_id                     = azurerm_subnet.oci_connect_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${element(azurerm_public_ip.oci_test_ip.*.id, count.index)}"
+    public_ip_address_id          = element(azurerm_public_ip.oci_test_ip.*.id, count.index)
   }
 }
 
 resource "azurerm_virtual_machine" "oci_testvm" {
-  count = "${var.arm_test_instances_count}"
+  count                 = var.arm_test_instances_count
   name                  = "oci-testvm-${count.index}"
-  location              = "${azurerm_resource_group.oci_connect.location}"
-  resource_group_name   = "${azurerm_resource_group.oci_connect.name}"
-  network_interface_ids = ["${element(azurerm_network_interface.oci_testvm_nic.*.id, count.index)}"]
+  location              = azurerm_resource_group.oci_connect.location
+  resource_group_name   = azurerm_resource_group.oci_connect.name
+  network_interface_ids = [element(azurerm_network_interface.oci_testvm_nic.*.id, count.index)]
   vm_size               = "Standard_DS1_v2"
 
   delete_os_disk_on_termination    = true
@@ -97,16 +96,17 @@ resource "azurerm_virtual_machine" "oci_testvm" {
     disable_password_authentication = false
 
     ssh_keys {
-      key_data = "${var.ssh_public_key}"
+      key_data = var.ssh_public_key
       path     = "/home/azure/.ssh/authorized_keys"
     }
   }
 }
 
 output "azure_vm_private_ip" {
-  value = ["${azurerm_network_interface.oci_testvm_nic.*.private_ip_address}"]
+  value = [azurerm_network_interface.oci_testvm_nic.*.private_ip_address]
 }
 
 output "azure_vm_public_ip" {
-  value = ["${azurerm_public_ip.oci_test_ip.*.ip_address}"]
+  value = [azurerm_public_ip.oci_test_ip.*.ip_address]
 }
+
--- variables.tf
+++ variables.tf
@@ -1,28 +1,59 @@
-variable "oci_tenancy_ocid" {}
-variable "oci_user_ocid" {}
-variable "oci_fingerprint" {}
-variable "oci_private_key_path" {}
-variable "oci_compartment_ocid" {}
-variable "oci_region" {}
+variable "oci_tenancy_ocid" {
+}
 
-variable "oci_cidr_vcn" {}
-variable "oci_cidr_subnet" {}
+variable "oci_user_ocid" {
+}
 
-variable "arm_subscription_id" {}
-variable "arm_client_id" {}
-variable "arm_client_secret" {}
-variable "arm_tenant_id" {}
-variable "arm_region" {}
+variable "oci_fingerprint" {
+}
 
-variable "arm_cidr_vnet" {}
-variable "arm_cidr_subnet" {}
-variable "arm_cidr_gw_subnet" {}
+variable "oci_private_key_path" {
+}
 
-variable "ssh_public_key" {}
+variable "oci_compartment_ocid" {
+}
 
-variable "peering_net" {}
+variable "oci_region" {
+}
 
-variable "oci_test_image" {}
+variable "oci_cidr_vcn" {
+}
+
+variable "oci_cidr_subnet" {
+}
+
+variable "arm_subscription_id" {
+}
+
+variable "arm_client_id" {
+}
+
+variable "arm_client_secret" {
+}
+
+variable "arm_tenant_id" {
+}
+
+variable "arm_region" {
+}
+
+variable "arm_cidr_vnet" {
+}
+
+variable "arm_cidr_subnet" {
+}
+
+variable "arm_cidr_gw_subnet" {
+}
+
+variable "ssh_public_key" {
+}
+
+variable "peering_net" {
+}
+
+variable "oci_test_image" {
+}
 
 variable "oci_test_instances_count" {
   default = 2
